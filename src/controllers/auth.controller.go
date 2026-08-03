bfc358ac7e478c043edf7595c2abe4c04a4b6d42
rsthopaz <thopazrosadi15@gmail.com>
2026-08-03 17:07:41 +0700

    add register controller

---
+++ src/controllers/auth.controller.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"main/src/models"
+	"os"
+	"main/src/db"
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func RegisterUser(c fiber.Ctx) error {
+	type request struct {
+		Email   string `json:"email"`
+		Password string `json:"password"`
+	}
+
+
+var body request
+
+if err := c.BodyParser(&body); err != nil {
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse request"})
+}
+
+var existingUser models.User
+
+err := db.DB.Collection("users").FindOne(c.Context(), bson.M{"email": body.Email}).Decode(&existingUser)
+if err == nil {
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "User already exists"})
+}
+
+hashPassword, _ := bcrypt.GenerateFromPassword([]byte(body.Password), 14)
+
+userDoc := bson.M{
+	"email": body.Email,
+	"password": string(hashPassword),
+}
+
+_, err = db.DB.Collection("users").InsertOne(c.Context(), userDoc)
+if err != nil {
+	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not create user"})
+}
+
+return c.JSON(fiber.Map{"message": "User registered successfully"})
+
+}
