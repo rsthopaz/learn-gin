9dc5fd9d9de35a7f94456f7c6fa28f7519e56979
rsthopaz <thopazrosadi15@gmail.com>
2026-08-03 16:30:15 +0700

    fixing setup

--- src/app.go
+++ src/app.go
@@ -1,12 +1,24 @@
 package src
 
 import (
-    "github.com/gofiber/fiber/v3"
+	"log"
+	"main/src/db"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
 )
 
 func SetupApp() *fiber.App {
 
 	app := fiber.New()
+
+	err := godotenv.Load()
+
+	if err != nil{
+		log.Fatal("Error Loading .env file")
+	}
+
+	db.ConnectDB()
 
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World!")
