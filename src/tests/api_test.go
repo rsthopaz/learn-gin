38002a2f66dd7509ae7ff35bdb198e6bc7d9542e
rsthopaz <thopazrosadi15@gmail.com>
2026-08-04 17:56:20 +0700

    add logout test

--- src/tests/api_test.go
+++ src/tests/api_test.go
@@ -3,22 +3,25 @@ package tests
 import (
 	// "main/src/controllers"
 	"main/src/routes"
+	"net/http"
 	"net/http/httptest"
- 	"github.com/gofiber/fiber/v3"
+
+	"github.com/gofiber/fiber/v3"
+
 	// "strings"
-	"encoding/json"
 	"bytes"
-	"testing"
+	"encoding/json"
 	"main/src/db"
 	"os"
+	"testing"
 )
 
 var testUser = struct{
 	Email string `json:"email"`
 	Password string `json:"password"`
 }{
-	Email: "testauto5@gmail.com",
-	Password: "passwordauto5",
+	Email: "testauto7@gmail.com",
+	Password: "passwordauto7",
 }
 
 func TestMain(m *testing.M) {
@@ -29,6 +32,8 @@ func TestMain(m *testing.M) {
 
 	os.Exit(code)
 }
+
+var jwtCookie *http.Cookie
 
 func TestRegister(t *testing.T){
 	app := fiber.New()
@@ -78,6 +83,35 @@ func TestLogin(t *testing.T){
 		"Content-Type",
 		"application/json",
 	)
+
+	resp, err := app.Test(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Error("expected 200")
+	}
+
+	cookies := resp.Cookies()
+
+	jwtCookie = cookies[0]
+
+}
+
+func TestLogout(t *testing.T){
+	app := fiber.New()
+
+	routes.AuthRoutes(app)
+
+	req := httptest.NewRequest(
+		"POST",
+		"/auth/logout",
+		nil,
+	)
+
+	req.AddCookie(jwtCookie)
 
 	resp, err := app.Test(req)
 
